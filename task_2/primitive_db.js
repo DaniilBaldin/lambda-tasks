3846c0509186cdb9850ab7d30b458f9deae00f52
Daniil Baldin <84784849+DaniilBaldin@users.noreply.github.com>
2022-06-10 12:56:25 +0300

    Revert "First commit"

---
+++ task_2/primitive_db.js
@@ -0,0 +1,71 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const firstQuestion = () => {
+  inquirer
+    .prompt({
+      type: "input",
+      name: "Name",
+      message: "Enter user's name. To cancel press ENTER.",
+    })
+    .then((answer) =>
+      answer.Name !== "" ? elseQuestions(answer) : searchQuestion()
+    );
+};
+
+const elseQuestions = (userInfo) => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "Gender",
+        message: "Select gender from the list:",
+        choices: ["male", "female", "other"],
+      },
+      { type: "input", name: "Age", message: "Enter your Age:" },
+    ])
+    .then((answers) => {
+      let userName = userInfo;
+      let answersInfo = answers;
+      let userData = Object.assign(userName, answersInfo);
+      fs.appendFileSync("database.txt", `${JSON.stringify(userData)}\n`);
+      firstQuestion();
+    });
+};
+
+searchQuestion = () => {
+  inquirer
+    .prompt({
+      type: "confirm",
+      name: "searchDB",
+      message: "Do you like to search User in DB?(y/n)",
+    })
+    .then((answer) => (answer.searchDB ? searchUser() : process.exit(0)));
+};
+
+const searchUser = () => {
+  let usersDB = fs.readFileSync("database.txt", "utf-8");
+  inquirer
+    .prompt({
+      type: "input",
+      name: "user",
+      message: " Enter username to search:",
+    })
+    .then((answer) => {
+      let result = usersDB
+        .split("\n")
+        .filter((e) => e !== "")
+        .map(JSON.parse);
+      let u = Object.values(answer);
+      console.log(u);
+      let res = result.filter((e) => u.includes(e.Name));
+      if (res.length) {
+        console.log(`User "${u}" is found! Search result:`);
+        console.log(res);
+      } else if (res == "") {
+        console.log("Please enter username!");
+      }
+    });
+};
+
+firstQuestion();
