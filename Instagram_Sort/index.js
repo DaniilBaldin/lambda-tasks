727aebcde4235f7ceb76ac5a9263f0491c7d2366
daniil <daniil_baldin@ukr.net>
2022-06-23 16:35:15 +0300

    First commit

---
+++ Instagram_Sort/index.js
@@ -0,0 +1,85 @@
+console.time('Time passed:');
+const fs = require('fs');
+
+async function uniqueValues() {
+    try {
+        let result = [];
+        for (let i = 0; i < 20; i++) {
+            const duplicates = await fs.readFileSync(`./src/out${i}.txt`, 'utf-8');
+            const unique = new Set(duplicates.split('\n'));
+            result.push(unique.size);
+        }
+        let sumResult = 0;
+        for (let item in result) {
+            sumResult += result[item];
+        }
+        console.log(`Unique values in all files: ${sumResult}`);
+    } catch {
+        (err) => {
+            if (err) console.log(err.message);
+        };
+    }
+}
+
+async function isInEveryFile() {
+    try {
+        let result = [];
+        for (let i = 0; i < 20; i++) {
+            const duplicates = await fs.readFileSync(`./src/out${i}.txt`, 'utf-8');
+            const unique = new Set(duplicates.split('\n'));
+            result.push(unique);
+        }
+        const uniqueElements = new Set(result.reduce((a, c) => a.concat([...c]), []));
+        let inEveryFile = [];
+        await uniqueElements.forEach((uniqueEl) => {
+            let counter = 0;
+            result.forEach((el) => {
+                if (el.has(uniqueEl)) {
+                    counter++;
+                    if (counter === 20) {
+                        inEveryFile.push(uniqueEl);
+                    }
+                }
+            });
+        });
+        console.log(`Number of unique combinations for 20 files: ${inEveryFile.length}`);
+    } catch {
+        (err) => {
+            if (err) console.log(err.message);
+        };
+    }
+}
+
+async function isInTenFiles() {
+    console.time();
+    try {
+        let result = [];
+        for (let i = 0; i < 20; i++) {
+            const duplicates = await fs.readFileSync(`./src/out${i}.txt`, 'utf-8');
+            const unique = new Set(duplicates.split('\n'));
+            result.push(unique);
+        }
+        const uniqueElements = new Set(result.reduce((a, c) => a.concat([...c]), []));
+        let inEveryFile = [];
+        await uniqueElements.forEach((uniqueEl) => {
+            let counter = 0;
+            result.forEach((el) => {
+                if (el.has(uniqueEl)) {
+                    counter++;
+                    if (counter === 10) {
+                        inEveryFile.push(uniqueEl);
+                    }
+                }
+            });
+        });
+        console.log(`Number of unique combinations for 10 files: ${inEveryFile.length}`);
+    } catch {
+        (err) => {
+            if (err) console.log(err.message);
+        };
+    }
+}
+uniqueValues();
+isInEveryFile();
+isInTenFiles();
+console.timeEnd('Time passed:');
